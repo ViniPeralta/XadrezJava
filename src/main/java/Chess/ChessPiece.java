b3d62a0b287b690194ffa0da5bfe7a36aaa26f59
Vinicius Peralta <vinicius-peralta@hotmail.com>
2021-07-06 21:14:57 -0300

    Implementado o tabuleiro sem peças

---
+++ src/main/java/Chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package Chess;
+
+import BoardLayer.Board;
+import BoardLayer.Piece;
+
+public class ChessPiece extends Piece {
+    
+    private Color color;
+
+    public ChessPiece(Color color, Board board) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
